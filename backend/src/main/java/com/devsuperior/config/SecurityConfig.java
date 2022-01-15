7ec1863d39ab40bb8a12ada37574d7e4fca93f8d
Joan Wilson <joanwmoliveira@gmail.com>
2022-01-15 01:44:22 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.devsuperior.backend.config;
+package com.devsuperior.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
