8a628b4a82913093209652a2405072ac4f282882
Joan Wilson <joanwmoliveira@gmail.com>
2022-01-16 17:54:33 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,17 +2,28 @@ import Pagination from 'components/Pagination';
 import MovieCard from 'components/MovieCard';
 import axios from 'axios';
 import { BASE_URL } from 'utils/requests';
+import { useEffect, useState } from 'react';
+import { MoviePage } from 'types/movie';
+
 
 function Listing() {
 
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
         .then(response => {
-            console.log(response.data);
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
         });
+    }, []);
+
+    
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
