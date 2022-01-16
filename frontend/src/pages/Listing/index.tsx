53046faee510f82a28e0142973bb5c5af92f4b25
Joan Wilson <joanwmoliveira@gmail.com>
2022-01-16 17:42:40 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,16 @@
 import Pagination from 'components/Pagination';
 import MovieCard from 'components/MovieCard';
+import axios from 'axios';
+import { BASE_URL } from 'utils/requests';
 
 function Listing() {
+
+    //FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        });
+
     return (
         <>
             <Pagination />
